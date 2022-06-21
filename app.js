c5ef361c570241d4c10ac0566a85d59e203a92c6
Duvelio <duvanbedo19@gmail.com>
2022-06-20 20:34:27 -0500

    cambios

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.set('puerto',process.env.PORT || 3000)
+
+
+
+app.listen(app.get('puerto'), ()=>console.log(`Servidor escuchando en puerto ${app.get('puerto')}`));
+
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
